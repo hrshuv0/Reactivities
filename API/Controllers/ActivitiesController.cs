2812ec6f5a40ec67fa9d2e2ca969b34e31380641
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-02 20:45:18 +0600

    added crud - activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,31 +1,36 @@
 ﻿using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
 
 namespace API.Controllers;
 
 public class ActivitiesController : BaseApiController
 {
-    private readonly IMediator _mediator;
-
-    public ActivitiesController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-
     // GET
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await _mediator.Send(new List.Query());
+        return await Mediator.Send(new List.Query());
     }
 
     [HttpGet("{id:guid}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return Ok();
+        return await Mediator.Send(new Details.Query {Id = id});
     }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity([FromBody] Activity activity)
+    {
+        return Ok(await Mediator.Send(new Create.Command{Activity = activity}));
+    }
+
+    [HttpPut("{id:guid}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+        activity.Id = id;
+        
+        return Ok(await Mediator.Send(new Edit.Command{Activity = activity}));
+    }
+    
 }
