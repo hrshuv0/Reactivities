0f5e2cb8ff111ba3f914cd9b87d609af52668087
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-03 00:40:47 +0600

    spa - added navigation

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,31 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 import axios from 'axios';
-import { Header, List } from 'semantic-ui-react';
+import { Container, Header, List } from 'semantic-ui-react';
+import { Activity } from './models/activity';
+import Navbar from './NavBar';
 
 function App() {
 
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     axios.get("http://localhost:5000/api/activities").then(response => {
-      
+
       setActivities(response.data);
     })
   }, [])
 
   return (
-    <div className="App">
-      <Header as='h2' icon='users' content='Reactivities'/>
-      
+    <>
+      <Navbar />
+      <Container style={{ marginTop: '7em' }}>
         <List>
-          {activities.map((activity: any) => (
+          {activities.map((activity) => (
             <List.Item key={activity.id}>
               {activity.title}
             </List.Item>
           ))}
         </List>
-      
-    </div>
+
+      </Container>
+
+    </>
   );
 }
 
