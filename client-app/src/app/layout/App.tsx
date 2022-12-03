c778039e522d1eb6ef557baf9d1d03cc1a0a8481
hrshuv0 <h.r.shuvo9@gmail.com>
2022-12-03 11:04:21 +0600

    spa -  added display edit form

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -9,6 +9,8 @@ function App() {
 
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
+
 
   useEffect(() => {
     axios.get("http://192.168.68.118:5000/api/activities").then(response => {
@@ -25,16 +27,27 @@ function App() {
     setSelectedActivity(undefined);
   }
 
+  function handleFormOpen(id?: string) {
+    id ? handleSelectedActivity(id) : handleCancelSelectedActivity();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
 
   return (
     <>
-      <Navbar />
+      <Navbar openForm={handleFormOpen} />
       <Container style={{ marginTop: '7em' }}>
         <ActivityDashboard
           activities={activities}
           selectedActivity={selectedActivity}
           selectActivity={handleSelectedActivity}
           cancelSelectActivity={handleCancelSelectedActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
         />
 
       </Container>
--- client-app/src/app/layout/NavBar.tsx
+++ client-app/src/app/layout/NavBar.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { Button, Container, Menu, MenuItem } from 'semantic-ui-react'
 
-export default function Navbar() {
+interface Props {
+    openForm: () => void;
+}
+
+export default function Navbar({ openForm }: Props) {
     return (
         <Menu inverted fixed='top'>
             <Container>
@@ -11,7 +15,7 @@ export default function Navbar() {
                 </Menu.Item>
                 <Menu.Item name='Activities' />
                 <Menu.Item>
-                    <Button positive content='Create Activity' />
+                    <Button positive content='Create Activity' onClick={openForm}/>
                 </Menu.Item>
             </Container>
         </Menu>
